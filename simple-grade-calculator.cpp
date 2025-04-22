b4ba441dc9cc0445ea9d3f4a1b4c94a4312f152a
harshachandel <harshachandel2007@gmail.com>
2025-04-22 23:19:47 +0530

    DSA Project 1 simple grade calc

---
+++ simple-grade-calculator.cpp
@@ -0,0 +1,51 @@
+
+// Simple Grade Calculator Using Switch Case  : --
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+
+//     cout<<"Creating a simple calcultor of two variables !"<<endl; //Heading 
+
+//     int num1; //Taking variable 
+//     cout<<"Enter the first number : ";
+//     cin>>num1; //Taking input from user 
+    
+//     char op;  //Taking operator 
+//     cout<<"Enter the opertor : ";
+//     cin>>op;  //Taking input from user 
+
+//     int num2; //Taking another variable 
+//     cout<<"Enter the first number : ";
+//     cin>>num2; //Taking input from user 
+
+//     switch(op){ //Switch case 
+
+//         case '+': //Conditon for sum 
+//         cout<<"The sum is "<<num1+num2;
+//         break;
+
+//         case '-': //Conditon for substraction
+//         cout<<"The substraction is "<<num1-num2;
+//         break;
+
+//         case '*': //Conditon for multiplication
+//         cout<<"The multiplication is "<<num1*num2;
+//         break;
+
+//         case '/': //Conditon for division
+//         cout<<"The division is "<<num1/num2;
+//         break;
+
+//         case '%': //Conditon for modulus
+//         cout<<"The modulus is "<<num1%num2;
+//         break;
+
+//         default: //Conditon for default
+//         cout<<"Please enter a valid opertor ! ";
+//         break;
+//     }
+
+//     return 0;
+// }
